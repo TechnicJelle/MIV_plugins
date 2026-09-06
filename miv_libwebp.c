1ae8dec80fa359ec12a31c6a8b1da862c27ab785
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-09-06 23:13:08 +0200

    Improve error messages a tad

--- miv_libwebp.c
+++ miv_libwebp.c
@@ -23,7 +23,7 @@ Log pre_render(Pre_Rendering_Info *pre_info) {
 	if (success == 0) {
 		return (Log){
 			.type = LOG_TYPE_ERROR,
-			.message = to_string("Invalid WebP file"),
+			.message = to_string("Invalid WebP file header"),
 		};
 	}
 
@@ -54,7 +54,7 @@ Log render(Pre_Rendering_Info *pre_info, Rendering_Info *render_info) {
 	if (data == NULL) {
 		return (Log){
 			.type = LOG_TYPE_ERROR,
-			.message = to_string("Invalid WebP file"),
+			.message = to_string("Invalid WebP file data"),
 		};
 	}
 
