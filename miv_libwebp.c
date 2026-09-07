f44adb0bc706d4598b06edd0ecc8e24df90a7e09
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-09-07 03:53:43 +0200

    Store WebP features across MIV functions

--- miv_libwebp.c
+++ miv_libwebp.c
@@ -20,13 +20,21 @@ void add_to_metadata(Pre_Rendering_Info *pre_info, char* key, char* value) {
 	pre_info->metadata[pre_info->metadata_count-1][1] = to_string(value);
 }
 
+typedef struct {
+	WebPBitstreamFeatures features;
+} PluginData;
+
 Log pre_render(Pre_Rendering_Info *pre_info) {
+	PluginData* plugin_data = malloc(sizeof(PluginData));
+	pre_info->user_ptr = plugin_data;
+
 	const size_t data_size = 32;
 	uint8_t file_data[data_size];
 	size_t read_size = fread(file_data, 1, data_size, pre_info->fileptr);
 
 	WebPBitstreamFeatures features;
 	VP8StatusCode status = WebPGetFeatures(file_data, read_size, &features);
+	plugin_data->features = features;
 	if (status != VP8_STATUS_OK) {
 		char* errs[8] = {
 			"Invalid WebP file header: OK (this should never happen!)",
@@ -58,6 +66,9 @@ Log pre_render(Pre_Rendering_Info *pre_info) {
 }
 
 Log render(Pre_Rendering_Info *pre_info, Rendering_Info *render_info) {
+	PluginData* plugin_data = pre_info->user_ptr;
+	WebPBitstreamFeatures* features = &plugin_data->features;
+
 	// How big is the file, anyway
 	fseek(pre_info->fileptr, 0, SEEK_END);
 	size_t file_size = ftell(pre_info->fileptr);
@@ -68,26 +79,36 @@ Log render(Pre_Rendering_Info *pre_info, Rendering_Info *render_info) {
 	size_t bytes_read = fread(file_data, 1, file_size, pre_info->fileptr);
 
 	// Decode file data to pixel data
-	int width, height;
-	uint8_t *data = WebPDecodeRGBA(file_data, bytes_read, &width, &height);
-	free(file_data);
+	if (!features->has_animation) {
+		int width, height;
+		uint8_t *data = WebPDecodeRGBA(file_data, bytes_read, &width, &height);
+		free(file_data);
 
-	// Process data if not NULL
-	if (data == NULL) {
+		// Process data if not NULL
+		if (data == NULL) {
+			return (Log){
+				.type = LOG_TYPE_ERROR,
+				.message = to_string("Invalid WebP file data"),
+			};
+		}
+
+		memcpy(render_info->buffer, data, width * height * 4);
+
+		WebPFree(data);
+	} else {
 		return (Log){
 			.type = LOG_TYPE_ERROR,
-			.message = to_string("Invalid WebP file data"),
+			.message = to_string("Animated WebP not supported yet."),
 		};
 	}
-
-	memcpy(render_info->buffer, data, width * height * 4);
-
-	WebPFree(data);
 
 	return (Log){0};
 }
 
 Log cleanup(Pre_Rendering_Info *pre_info) {
-	(void)pre_info; //shut up about the unused variable...
+	if (pre_info->user_ptr != NULL) {
+		free(pre_info->user_ptr);
+		pre_info->user_ptr = NULL;
+	}
 	return (Log){0};
 }
