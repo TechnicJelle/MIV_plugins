79674d7a41fb4f9c77a1296f786152db365d1bad
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-09-09 06:47:32 +0200

    Add `clean` option to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "$1" == "clean" ]]; then
+	rm -f stbi.o stbi_png.o miv_stbi.so miv_libwebp.so miv_zipkra.so
+	exit
+fi
+
 if [ ! -f stbi.o ]; then
 	gcc -fPIC -std=gnu11 -O3 stbi.c -c -o stbi.o
 	gcc -fPIC -std=gnu11 -O3 stbi_png.c -c -o stbi_png.o
