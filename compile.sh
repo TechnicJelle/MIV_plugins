e471d8f4e945519b62f38199a82621e1042fcdac
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-09-09 02:25:56 +0200

    Use GNU-C11 standard instead of C11

--- compile.sh
+++ compile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ ! -f stbi.o ]; then
-	gcc -fPIC -std=c11 -O3 stbi.c -c -o stbi.o
+	gcc -fPIC -std=gnu11 -O3 stbi.c -c -o stbi.o
 fi
 
 if [[ "$1" == "release" ]]; then
-	gcc -fPIC -std=c11 -O3 miv_stbi.c stbi.o -shared -o miv_stbi.so
-	gcc -fPIC -std=c11 -O3 miv_libwebp.c -shared -o miv_libwebp.so -lwebp -lwebpdemux
+	gcc -fPIC -std=gnu11 -O3 miv_stbi.c stbi.o -shared -o miv_stbi.so
+	gcc -fPIC -std=gnu11 -O3 miv_libwebp.c -shared -o miv_libwebp.so -lwebp -lwebpdemux
 else
-	gcc -fPIC -std=c11 -g miv_stbi.c stbi.o -shared -o miv_stbi.so
-	gcc -fPIC -std=c11 -g miv_libwebp.c -shared -o miv_libwebp.so -lwebp -lwebpdemux
+	gcc -fPIC -std=gnu11 -g miv_stbi.c stbi.o -shared -o miv_stbi.so
+	gcc -fPIC -std=gnu11 -g miv_libwebp.c -shared -o miv_libwebp.so -lwebp -lwebpdemux
 fi
