01db15034493b36641868acc45bac41136ecca3a
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-09-09 02:24:43 +0200

    Enable debug symbols in non-release compiles

--- compile.sh
+++ compile.sh
@@ -8,6 +8,6 @@ if [[ "$1" == "release" ]]; then
 	gcc -fPIC -std=c11 -O3 miv_stbi.c stbi.o -shared -o miv_stbi.so
 	gcc -fPIC -std=c11 -O3 miv_libwebp.c -shared -o miv_libwebp.so -lwebp -lwebpdemux
 else
-	gcc -fPIC -std=c11 miv_stbi.c stbi.o -shared -o miv_stbi.so
-	gcc -fPIC -std=c11 miv_libwebp.c -shared -o miv_libwebp.so -lwebp -lwebpdemux
+	gcc -fPIC -std=c11 -g miv_stbi.c stbi.o -shared -o miv_stbi.so
+	gcc -fPIC -std=c11 -g miv_libwebp.c -shared -o miv_libwebp.so -lwebp -lwebpdemux
 fi
